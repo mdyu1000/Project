4e03e5bbeac5d2a16d20d48613990610ca57f45d
KC <mdyu1000@gmail.com>
2018-06-12 01:35:00 +0800

    修正語言選項與badge的css

--- client/src/component/InputText.js
+++ client/src/component/InputText.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import { Col, FormGroup, Input } from 'reactstrap';
 
+const languageSeleteStyle = {
+  background: "#6c757d",
+  opacity: ".3",
+  color: "white",
+  borderRadius: "5px",
+  paddingLeft: ".5rem",
+  paddingRight: ".5rem",
+}
+
+const badgeStyle = {
+  boxShadow: "none",
+  backgroundColor: "#e8e8e8"
+}
+
 const NameBadge = (props) => {
   const badge = Object.entries(props.lists).map(([key, value]) => {
     return(
-      <span className="badge badge-primary mt-2 ml-2">
+      <span className="badge mt-2 ml-2" style={ badgeStyle }>
+        {key.toString() + " | "}
         {value.toString()}
         <i language={key} style={{ cursor: "pointer", color: "rgba(0, 0, 0, .2)"}} 
           class="fa fa-remove my-auto ml-1" onClick={props.onDel}></i>
@@ -19,7 +34,6 @@ const NameBadge = (props) => {
 export default class InputText extends React.Component {
 	constructor(props){
 		super(props);
-    
 	}
 
   handleClickAdd = () => {
@@ -48,12 +62,12 @@ export default class InputText extends React.Component {
 	      <span>{this.props.title}</span>
 	      <div className="input-group mt-2">
 	      	<div className="input-group-prepend">
-	      		<select id={this.props.name + "Language"} className="browser-default">
-      				<option disabled selected value className="d-none"></option>
-	      			<option value="ch">Chinese</option>
-	      			<option value="en">English</option>
-              <option value="kr">Korea</option>
-              <option value="jp">Japan</option>
+	      		<select style={ languageSeleteStyle } id={this.props.name + "Language"} className="browser-default mt-2">
+      				<option disabled selected value className="d-none p-3"></option>
+	      			<option value="ch" className="m-5">Chinese</option>
+	      			<option value="en" className="m-3">English</option>
+              <option value="kr" className="m-3">Korea</option>
+              <option value="jp" className="m-3">Japan</option>
             </select>
 	      	</div>
 	      	<Input type="text" id={this.props.name + "Name"} name={this.props.name} style={{ height: "1rem" }} />
