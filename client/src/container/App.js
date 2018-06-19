24f349fbb6d3fa676ebcfd33415414e7ffff70e1
KC <mdyu1000@gmail.com>
2018-06-20 00:50:43 +0800

    暫時處理好router

--- client/src/container/App.js
+++ client/src/container/App.js
@@ -2,14 +2,17 @@ import React, { Component } from 'react';
 import NavgationBar from '../component/Navbar';
 import SideNav from '../component/SideNav';
 import NewRoute from './NewRoute';
+import { Route, HashRouter, BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div>
-      	<NavgationBar />
-      	<SideNav />
-        <NewRoute />
+        <NavgationBar />
+        <SideNav />
+        <BrowserRouter>
+          <Route exact path="/" component={NewRoute} />
+        </BrowserRouter> 
       </div>
     );
   }
